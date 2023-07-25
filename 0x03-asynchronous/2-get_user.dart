ee665eb0fc4ddec29fbfe8f82cfce0eb2c4c9565
fedy69 <>
2023-07-25 21:57:09 +0100

    get user

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,8 +1,9 @@
 import '2-util.dart';
+import 'dart:async';
 
 Future<void> getUser() async {
   try {
-    String userData = await fetchUserData();
+    String userData = await fetchUser();
     print(userData);
   } catch (error) {
     print("error caught: $error");
