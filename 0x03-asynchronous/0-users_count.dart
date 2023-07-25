d6e0eb859ed9bc96ea6b8a2bef24ec4f95b1c506
fedy69 <>
2023-07-25 21:44:42 +0100

    count

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import '0-util.dart';
+
+Future<void> usersCount() async {
+  int count = await fetchUsersCount();
+  print(count);
+}
+
 Future<int> fetchUsersCount() => Future.delayed(
       const Duration(seconds: 2),
       () => 19,
-    );
-
-usersCount() async {
-  print(await fetchUsersCount());
-}
+);
