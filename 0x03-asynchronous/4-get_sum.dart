9cbfbc7db6e1af13b0615232a0fa863ec7729097
fedy69 <>
2023-07-25 22:13:35 +0100

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,20 @@
+import "4-util.dart";
+import "dart:convert";
+
+Future<double> calculateTotal() async {
+  try {
+    final user = await fetchUserData();
+    final id = json.decode(user)["id"];
+    final products = await fetchUserOrders(id);
+
+    double totalPrice = 0.0;
+    for (String product in json.decode(products)) {
+      final price = await fetchProductPrice(product);
+      totalPrice += json.decode(price);
+    }
+
+    return totalPrice;
+  } catch (e) {
+    return -1;
+  }
+}
